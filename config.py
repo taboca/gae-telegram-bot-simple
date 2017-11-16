41370d750dab90d9613ed0f506ae0dd1e0c6ab47
Marcio Galli <mgalli@taboca.com>
2017-11-16 11:49:59 -0200

    No local config, actual sample config

--- config.py
+++
@@ -1,2 +0,0 @@
-TELEGRAM_API_TOKEN = 'TOKEN'
-URL_WEB_HOOK       = 'https://bot-telegram-gae-test.appspot.com/telegram_post_hook'
---
+++ sample-config.py
@@ -0,0 +1,4 @@
+# Rename this to config.py
+
+TELEGRAM_API_TOKEN = 'replace_this_string_with_token_from_telegram_bot_father'
+URL_WEB_HOOK       = 'https://bot-telegram-gae-test.appspot.com/telegram_post_hook'
